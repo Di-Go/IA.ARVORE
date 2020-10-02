7584236e64cff33d2e200f78029a2ca5a26f5d61
Di-Go <alunodiego360@gmail.com>
2020-10-01 22:50:50 -0300

    MAIN - Implementa método de inserção de valores na arvore

--- ImplementandoArvore/src/Arvore.java
+++ ImplementandoArvore/src/Arvore.java
@@ -1,4 +1,15 @@
 
 public class Arvore {
+	private No Raiz;
 	
+	public void Insira(int valor) {
+		Raiz = Insira(Raiz, valor);
+	}
+	
+	private No Insira(No no, int valor) {
+		if (no == null) {
+			return new No(valor);
+		}
+		return new No(valor);
+	}
 }
