1d12bf589c0856a84a6161c3335cf91fff17a998
Di-Go <alunodiego360@gmail.com>
2020-10-09 19:18:15 -0300

    MAIN - Ajusta inserção na arvore Implementa impressão de nós

--- ImplementandoArvore/src/Arvore.java
+++ ImplementandoArvore/src/Arvore.java
@@ -8,12 +8,59 @@ public class Arvore {
 	
 	private No Insira(No no, int valor) {
 		if (no != null) {
-			if (no.Valor < valor || no.Valor == valor) {
+			if (valor < no.Valor || no.Valor == valor) {
 				no.Esquerdo = Insira(no.Esquerdo, valor);
 			}else {
 				no.Direito = Insira(no.Direito, valor);
 			}
-		}		
-		return new No(valor);
+		}else {
+			return new No(valor);
+		}
+		return no;
+	}
+	
+	public void ImprimaPreOrdem() {
+		System.out.println("Pré Ordem:");
+		ImprimaPreOrdem(this.Raiz);
+	}
+	
+	private void ImprimaPreOrdem(No noAtual) {
+		if (noAtual == null) {
+			return;
+		}
+		
+		System.out.println(noAtual.Valor);
+		ImprimaPreOrdem(noAtual.Esquerdo);
+		ImprimaPreOrdem(noAtual.Direito);
+	}
+	
+	public void ImprimaEmOrdem() {
+		System.out.println("Em Ordem:");
+		ImprimaEmOrdem(this.Raiz);
+	}
+	
+	private void ImprimaEmOrdem(No noAtual) {
+		if (noAtual == null) {
+			return;
+		}
+		
+		ImprimaEmOrdem(noAtual.Esquerdo);
+		System.out.println(noAtual.Valor);
+		ImprimaEmOrdem(noAtual.Direito);
+	}
+	
+	public void ImprimaPosOrdem() {
+		System.out.println("Pós Ordem:");
+		ImprimaPosOrdem(this.Raiz);
+	}
+	
+	private void ImprimaPosOrdem(No noAtual) {
+		if (noAtual == null) {
+			return;
+		}
+		
+		ImprimaPosOrdem(noAtual.Esquerdo);
+		ImprimaPosOrdem(noAtual.Direito);
+		System.out.println(noAtual.Valor);
 	}
 }
--- ImplementandoArvore/src/Principal.java
+++ ImplementandoArvore/src/Principal.java
@@ -4,9 +4,15 @@ public class Principal {
 	public static void main(String[] args) {
 		Arvore arvore = new Arvore();
 		
+		arvore.Insira(4);
+		arvore.Insira(5);
+		arvore.Insira(6);
+		arvore.Insira(2);
+		arvore.Insira(3);
 		arvore.Insira(1);
 		
-		System.out.println("Teste");
+		arvore.ImprimaPreOrdem();
+		arvore.ImprimaEmOrdem();
+		arvore.ImprimaPosOrdem();
 	}
-
 }
